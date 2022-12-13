71380d8e46f8ae0fd1a71a97208fd4b306526f63
houze <ljhao177@outlook.com>
2022-12-13 10:19:52 +0800

    bouncing balls with evilcircle

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const para = document.querySelector('p');
+let count = 0;
+
 // 设置画布
 
 const canvas = document.querySelector('canvas');
@@ -8,29 +11,117 @@ const height = canvas.height = window.innerHeight;
 
 // 生成随机数的函数
 
-function random(min,max) {
+function random(min, max) {
   const num = Math.floor(Math.random() * (max - min)) + min;
   return num;
 }
 
 // 生成随机颜色值函数
 function randomColor() {
-  // return 'rgb(' + random(0, 255) + ', ' + random(0, 255) + ', ' + random(0, 255) + ')';
-  const color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
-  return color;
+  return 'rgb(' + random(0, 255) + ', ' + random(0, 255) + ', ' + random(0, 255) + ')';
+  // const color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+  // return color;
 }
 
-// 定义Ball构造器
-function Ball(x, y, velX, velY, color, size) {
+// 定义Ball继承自Shape
+class Ball extends Shape {
+  constructor(x, y, velX, velY, exists, color, size) {
+    super(x, y, velX, velY, exists);
+    this.color = color;
+    this.size = size;
+  }
+}
+
+// 继承自Shape
+class evilCircle extends Shape {
+  constructor(x, y, velX, velY, exists, color, size) {
+    super(x, y);
+    this.velX = velX = 20;
+    this.velY = velY = 20;
+    this.exists = exists;
+    this.color = color = 'white';
+    this.size = size = 10;
+  }
+}
+
+evilCircle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.strokeStyle = this.color;
+  ctx.lineWidth = 3;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+};
+
+evilCircle.prototype.checkBounds = function () {
+  if ((this.x + this.size) >= width) {
+    this.x -= this.size;
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x += this.size;
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.y -= this.size;
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.y += this.size;
+  }
+};
+
+evilCircle.prototype.setControls = function () {
+  window.onkeydown = e => {
+    switch (e.key) {
+      case 'a':
+      case 'A':
+      case 'ArrowLeft':
+        this.x -= this.velX;
+        break;
+      case 'd':
+      case 'D':
+      case 'ArrowRight':
+        this.x += this.velX;
+        break;
+      case 'w':
+      case 'W':
+      case 'ArrowUp':
+        this.y -= this.velY;
+        break;
+      case 's':
+      case 'S':
+      case 'ArrowDown':
+        this.y += this.velY;
+        break;
+    }
+  };
+};
+
+evilCircle.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (balls[j].exists) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists = false;
+        count--;
+        para.textContent = '剩余彩球数：' + count;
+      }
+    }
+  }
+};
+
+function Shape(x, y, velX, velY, exists) {
   this.x = x;
   this.y = y;
   this.velX = velX;
   this.velY = velY;
-  this.color = color;
-  this.size = size;
+  this.exists = exists;
 }
 
-Ball.prototype.draw = function() {
+Ball.prototype.draw = function () {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
@@ -38,12 +129,12 @@ Ball.prototype.draw = function() {
 };
 
 
-Ball.prototype.collisionDetect = function() {
+Ball.prototype.collisionDetect = function () {
   for (let j = 0; j < balls.length; j++) {
     if (this !== balls[j]) {
       const dx = this.x - balls[j].x;
       const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy *dy);
+      const distance = Math.sqrt(dx * dx + dy * dy);
 
       if (distance < this.size + balls[j].size) {
         balls[j].color = this.color = randomColor();
@@ -52,7 +143,7 @@ Ball.prototype.collisionDetect = function() {
   }
 };
 
-Ball.prototype.update = function() {
+Ball.prototype.update = function () {
   if ((this.x + this.size) >= width) {
     this.velX = -(this.velX);
   }
@@ -75,29 +166,41 @@ Ball.prototype.update = function() {
 
 let balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 100) {
   let size = random(10, 20);
   let ball = new Ball(
     random(0 + size, width - size),
     random(0 + size, height - size),
-    // random(0 + size, height - size),
     random(-7, 7),
     random(-7, 7),
+    true,
     randomColor(),
     size
   );
   balls.push(ball);
+  count++;
+  para.textContent = '剩余彩球数：' + count;
+  console.log(para.textContent);
 }
+
+let circle = new evilCircle(10, 10, true);
+circle.setControls();
 
 function loop() {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
   ctx.fillRect(0, 0, width, height);
 
   for (let i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
+    if (balls[i].exists) {
+      balls[i].draw();
+      balls[i].update();
+      balls[i].collisionDetect();
+    }
   }
+
+  circle.draw();
+  circle.checkBounds();
+  circle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
